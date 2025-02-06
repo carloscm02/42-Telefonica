42ec0c5991659e7266858874fcef3163885bab73
carlos <carloscamaras002@gmail.com>
2025-02-06 15:33:10 +0100

    con ayuda de raulin

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -30,50 +30,65 @@ char	*ft_strdup(const char *s)
 	return (str);
 }
 
-char	*ft_strjoin(char *crl, char *s2)
+char	*ft_strjoin(const char *s1, const char *s2)
 {
 	char	*joined_str;
 	size_t	i;
 	size_t	j;
 
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
 	if (!s2)
-		return (free(s2), NULL);
-	joined_str = malloc(sizeof(char) * (ft_strlen(crl) + ft_strlen(s2) + 1));
+		return (ft_strdup(s1));
+	joined_str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!joined_str)
 		return (NULL);
 	i = 0;
-	while (crl && crl[i])
+	while (s1[i])
 	{
-		joined_str[i] = crl[i];
+		joined_str[i] = s1[i];
 		i++;
 	}
 	j = 0;
 	while (s2[j])
 		joined_str[i++] = s2[j++];
 	joined_str[i] = '\0';
-	free(crl);
 	return (joined_str);
 }
 
 char	*update_position(int fd, char *current_line)
 {
 	char	*buffer;
+	char	*temp;
 	int		bytes_read;
 
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
-		return (free(current_line), NULL);
+	{
+		free(current_line);
+		return (NULL);
+	}
 	bytes_read = 1;
 	while (!ft_strchr(current_line, '\n') && bytes_read > 0)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_read == -1)
-			return (free(buffer), free(current_line), NULL);
-		if (bytes_read >= 0)
-			buffer[bytes_read] = '\0';
-		current_line = ft_strjoin(current_line, buffer);
+		{
+			free(buffer);
+			free(current_line);
+			return (NULL);
+		}
+		buffer[bytes_read] = '\0';
+		temp = ft_strjoin(current_line, buffer);
+		free(current_line);
+		current_line = temp; 
 		if (!current_line)
-			return (free(buffer), free(current_line), NULL);
+		{
+			free(buffer);
+			return (NULL);
+		}
 	}
 	free(buffer);
 	return (current_line);
@@ -83,20 +98,37 @@ char	*get_next_line(int fd)
 {
 	static char	*current_line = NULL;
 	char		*line;
+	char		*temp;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	current_line = update_position(fd, current_line);
-	if (!current_line || !*current_line)
+	if (!current_line)
 		return (NULL);
+	if (!*current_line)
+	{
+		free(current_line);
+		current_line = NULL;
+		return (NULL);
+	}
 	line = ft_strdup(current_line);
 	if (!line)
-		return (free(current_line), NULL);
+	{
+		free(current_line);
+		current_line = NULL;
+		return (NULL);
+	}
+	
 	if (ft_strchr(current_line, '\n'))
 	{
-		current_line = ft_strdup(ft_strchr(current_line, '\n') + 1);
+		temp = ft_strdup(ft_strchr(current_line, '\n') + 1);
+		free(current_line);
+		current_line = temp;
 		if (!current_line)
-			return (free(line), NULL);
+		{
+			free(line);
+			return (NULL);
+		}
 		line[ft_strlen(line) - ft_strlen(current_line)] = '\0';
 	}
 	else
